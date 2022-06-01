25d8d828654f2132c6527904e37387ebb06d035a
Snisarenko21 <sepalena@gmail.com>
2022-06-01 19:18:09 +0300

    checkboxChange

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import menuItem from './templates/menuitem.hbs';
 const refs = {
     list: document.querySelector('.js-menu'),
     checkbox: document.querySelector('#theme-switch-toggle'),
-
+    
 };
+
+checkboxChange();
 
 const Theme = {
   LIGHT: 'light-theme',
@@ -22,7 +24,15 @@ createMarkup(menu);
 
 refs.checkbox.addEventListener('change', oncheckboxChange);
 
-function oncheckboxChange() {
+function oncheckboxChange(e) {
+    e.preventDefault();
+    
+    const change = e.currentTarget.value;
+
+    localStorage.setItem('LOCALSTORAGE_KEY', change);
+
+    // e.currentTarget.reset();
+
     if (!refs.checkbox.checked) {
         document.body.classList.add(Theme.LIGHT);
         document.body.classList.remove(Theme.DARK);
@@ -30,7 +40,14 @@ function oncheckboxChange() {
     else {
         document.body.classList.remove(Theme.LIGHT);
         document.body.classList.add(Theme.DARK);
-        
     }
-        
 }
+    
+function checkboxChange() {
+    const updateChecked = localStorage.getItem('LOCALSTORAGE_KEY');
+    if (updateChecked) {
+        console.log(updateChecked);
+        refs.checkbox.value = updateChecked;
+    }
+}
+
